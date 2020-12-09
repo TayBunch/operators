b1c2d6a9125c2c34e942eb2301d3757d50f134f8
my.github.bunchtaylor30@gmail.com <my.github.bunchtaylor30@gmail.com>
2020-12-09 15:30:01 -0600

    commit tree 8a564bbf40573df20c7f019f18382fdeff693240

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+    if speed > 60 || speed < 40
+        true
+    else 
+        false
+    end   
 end
 
 
 
 def not_safe?(speed)
-	
-end
+     (speed) ? speed > 60 || speed < 40 : false 
+    end
 	
 
 
